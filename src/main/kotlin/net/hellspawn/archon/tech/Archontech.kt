2cec44337c54934bab81360fdee86c00b8ffb2a4
Anna Thorne <annathorne@pm.me>
2025-09-27 18:34:21 +0200

    add base

---
+++ src/main/kotlin/net/hellspawn/archon/tech/Archontech.kt
@@ -0,0 +1,15 @@
+package net.hellspawn.archon.tech
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Archontech : ModInitializer {
+    private val logger = LoggerFactory.getLogger("archon-tech")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
---
+++ src/main/kotlin/net/hellspawn/archon/tech/ArchontechDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.hellspawn.archon.tech
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ArchontechDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
